fe755ad01783e6b2d1af984eb62e692a4836b610
y3owk1n <wongyeowkin@gmail.com>
2024-01-06 18:20:38 +0800

    chore: refactor mkdirx for consistent completion patterns

--- completions/mkdirx.fish
+++ completions/mkdirx.fish
@@ -2,10 +2,13 @@ complete mkdirx --no-files
 
 function __mkdirx_folder_complete
     set -l options
-    for option in (fd -t d -d 1)
-        set options $options (basename $option)
+    for option in (command ls)
+        if test -d $option
+            set options $options $option
+        end
     end
     echo $options
 end
 
-complete -c mkdirx -n __fish_use_subcommand -s d -l directory -a "(__mkdirx_folder_complete)" -d "Directory name"
+complete -c mkdirx -a "(__mkdirx_folder_complete)" -d "Directory name" -r
+complete -c mkdirx -a "(__mkdirx_folder_complete)" -d "Directory name" -r -f
